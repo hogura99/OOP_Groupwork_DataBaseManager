eecfc3c9d9f40c7d6fe789d9ce74887cf397fd5b
blackclouds <1667456022@qq.com>
2019-04-09 20:45:26 +0800

    Added Data.h Base.h Derived.h

---
+++ src/Derived.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Base.h"
+
+#include <string>
+
+class dataInt : public Base{
+    int data;
+    public:
+        dataInt();
+        ~dataInt();
+
+        dataInt* setData(int value) override;
+        int getData();
+        std::string getTypename() override;
+}
+
+class dataDouble : public Base{
+    double data;
+    public:
+        dataDouble();
+        ~dataDouble();
+
+        dataDouble* setData(double value) override;
+        double getData();
+        std::string getTypename() override;
+}
+
+class dataString : public Base{
+    std::string data;
+    public:
+        dataString();
+        ~dataString();
+        
+        datastd::String* setData(std::string value) override;
+        std::string getData();
+        std::string getTypename() override;
+}
